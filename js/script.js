b0dd2ab2cb01316dfd0fec97d61c777e930f163f
jose luis <130926408+Skulred@users.noreply.github.com>
2026-06-10 14:19:06 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-function toggleTeam(button)
-{
-    const children =
-        button.closest('.node')
-              .querySelector('.children');
-
+function toggleTeam(button) {
+   
+    const card = button.closest('.card');
+    '
+    const children = card.nextElementSibling;
+    
     if (!children) return;
 
     children.classList.toggle('hidden');
 
-    button.innerHTML =
-        children.classList.contains('hidden')
-            ? '▼ Expandir equipo'
-            : '▲ Contraer equipo';
+    button.innerHTML = children.classList.contains('hidden') 
+        ? '▼ Expandir equipo' 
+        : '▲ Contraer equipo';
 }
